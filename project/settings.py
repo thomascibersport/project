151d8c80aed8adf805c3387f5f6c327651322d39
root <thomascibersport@gmail.com>
2024-11-27 22:17:31 +0300

    1

--- project/settings.py
+++ project/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
